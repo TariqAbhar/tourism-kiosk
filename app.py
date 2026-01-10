daf91f26562814a88672205132f9982efae88253
ARIS <aidilaris32@gmail.com>
2026-01-10 10:49:12 +0800

    Fixed folder structure

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# --- PAGE ROUTING ---
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/destinations')
+def destinations():
+    return render_template('destinations.html')
+
+@app.route('/culture')
+def culture():
+    return render_template('culture.html')
+
+@app.route('/plan')
+def plan():
+    return render_template('plan.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
